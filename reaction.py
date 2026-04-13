5c4d3dce94ce390da1f9f38d2929e889cc2143d5
Hu Hantao <>
2026-04-13 20:14:48 +0800

    step4-player-name-exit

--- reaction.py
+++ reaction.py
@@ -1,18 +1,27 @@
-#3
+#4
 from gpiozero import LED, Button
 from time import sleep
 from random import uniform
+import sys
 
 led = LED(4)
-right_button = Button(15)
 left_button = Button(14)
+right_button = Button(15)
 
+left_name = input('left player name is: ')
+right_name = input('right player name is: ')
+
+print("Game start! Wait for LED off...")
 led.on()
 sleep(uniform(5, 10))
 led.off()
 
 def pressed(button):
-    print(str(button.pin.number) + ' won the game')
+    if button.pin.number == 14:
+        print(left_name + ' won the game!')
+    else:
+        print(right_name + ' won the game!')
+    sys.exit()
 
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
